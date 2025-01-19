c9cd585e4ebaee12dbb7b533001cf92a1751aa3c
borz7zy <borz7zy@yandex.ru>
2025-01-20 00:39:59 +0100

    добавлена строка порта сервера mysql

--- generate_mysql.py
+++ generate_mysql.py
@@ -14,15 +14,20 @@ mysql_host = os.getenv('DDP_MYSQL_HOST')
 mysql_user = os.getenv('DDP_MYSQL_USER')
 mysql_pass = os.getenv('DDP_MYSQL_PASS')
 mysql_dtbs = os.getenv('DDP_MYSQL_DTBS')
+mysql_port = int(os.getenv('DDP_MYSQL_PORT'))
 
 if not mysql_host or not mysql_user or not mysql_pass or not mysql_dtbs:
     raise ValueError("One of the required environment variable values ​​is not set!")
+
+if not mysql_port:
+    mysql_port = 3306
 
 config_content = f"""
 hostname = {mysql_host}
 username = {mysql_user}
 password = {mysql_pass}
 database = {mysql_dtbs}
+server_port = {mysql_port}
 """
 
 with open(file_path, 'w') as config_file:
